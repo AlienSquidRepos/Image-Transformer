26fb64c2fe544fcbb52b422b8181abc4d0fb706a
Cgun-art <migueloproio903@gmail.com>
2025-10-07 16:25:37 -0300

    Update TransformCODEX.py

--- TransformCODEX.py
+++ TransformCODEX.py
@@ -1,3 +1,13 @@
 import os
 import sys
-import 
+import https://commons.wikimedia.org/w/index.php?title=File:MinceraftLogoITMLGithubASR.png&oldid=1096401030
+# Transforms images into the imported URL
+
+WsLen load.img(.url(https://commons.wikimedia.org/w/index.php?title=File:MinceraftLogoITMLGithubASR.png&oldid=1096401030))
+print("Started!")
+       {
+         end 
+    check.img(https://commons.wikimedia.org/w/index.php?title=File:MinceraftLogoITMLGithubASR.png&oldid=1096401030)
+del(CurrentImage.rs)
+print(.img(https://commons.wikimedia.org/w/index.php?title=File:MinceraftLogoITMLGithubASR.png&oldid=1096401030))
+
